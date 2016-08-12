945afcd1aaa09c175a075747203e2cdf8adcaf45
derek <d1du0191@gmail.com>
2016-08-12 11:42:36 -0700

    Update AmenitiesUpdateRequest.cs

--- RapidRents/Models/Amenities/AmenitiesUpdateRequest.cs
+++ RapidRents/Models/Amenities/AmenitiesUpdateRequest.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
-using Sabio.Web.Models.Requests.Amenities;
+using RapidRents.Web.Models.Requests.Amenities;
 
-namespace Sabio.Web.Models.Requests.Amenities
+namespace RapidRents.Web.Models.Requests.Amenities
 {
     public class AmenitiesUpdateRequest : AmenitiesAddRequest
     {
