68720e51d867dd302852ec1b9ea5952ce826c859
derek <d1du0191@gmail.com>
2016-08-12 11:37:42 -0700

    Update AmenitiesController.cs

--- RapidRents/Controllers/AmenitiesController.cs
+++ RapidRents/Controllers/AmenitiesController.cs
@@ -1,11 +1,11 @@
-﻿using Sabio.Web.Models.ViewModels;
+﻿using RapidRents.Web.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Sabio.Web.Controllers
+namespace RapidRents.Web.Controllers
 {
     [Authorize]
     [RoutePrefix("amenities")]
